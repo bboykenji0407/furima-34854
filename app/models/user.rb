a0f65af1baaf47aa7588bc0e19c6b0daa6237dc0
bboykenji0407 <bboy.kenji.flex@gmail.com>
2021-03-19 18:44:16 +0900

    コードの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-        validates :nickname, presence: true
-        with_options presence: true, format: { with: /\A[ー-龥]+\z/, message: '全角文字を使用してください'} do
-          validates :first_name, presence:true
-          validates :last_name, presence: true
-        end
-        with_options presence:true, format: { with: /\A[ぁ-んァ-ヶー－]+\z/, message: '全角カナを使用してください'} do
-          validates :first_name_kana, presence: true
-          validates :last_name_kana, presence: true
-        end
-        validates :date_of_birth, presence: true
+  validates :nickname, presence: true
+  with_options presence: true, format: { with: /\A[ー-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+  end
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶー－]+\z/, message: '全角カナを使用してください' } do
+    validates :first_name_kana, presence: true
+    validates :last_name_kana, presence: true
+  end
+  validates :date_of_birth, presence: true
 end
