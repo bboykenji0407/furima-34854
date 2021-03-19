cd02c3ac05789e9af359e29bb1b4cfb0c25d5e0b
bboykenji0407 <bboy.kenji.flex@gmail.com>
2021-03-19 17:47:29 +0900

    ユーザー新規登録機能の実装

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname               {Faker::Name.initials(number: 2)}
+    email                  {Faker::Internet.free_email}
+    password               {Faker::Internet.password(min_length: 6)}
+    password_confirmation  {password}
+    first_name             {'鈴木'}
+    last_name              {'大将'}
+    first_name_kana        {'カナ'}
+    last_name_kana         {'カナ'}
+    date_of_birth          {'1992-04-07'}
+  end
+end
